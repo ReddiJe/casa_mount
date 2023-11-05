a6faa0c85e06582798a815a1efb841e7a5e140ae
Alabaich <enjoyable.design@gmail.com>
2023-11-05 14:01:54 -0500

    modified:   themes/hello-elementor/allNews.php

--- themes/hello-elementor/allNews.php
+++ themes/hello-elementor/allNews.php
@@ -3,7 +3,7 @@
 ?>
 
 <?php get_header(); 
-
+?>
 
 <h2><?php the_title();?></h2>
 
@@ -36,58 +36,42 @@ $postslist = get_posts([
 
     .containerNews {
         padding: 25px;
-        grid-column-gap: 25px;
-        grid-row-gap: 25px;
-        height: 80vh;
         width: 100vw;
         display: flex;
-        grid-template-columns: repeat(3, 1fr);
-        grid-template-rows: repeat(2, 1fr);
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        gap: 25px;
 }
 
-    // .h1 {
-    //     font-size: 1.5rem;
-    //     text-decoration: none;
-    //     font-family: Urbanist;
-    //     font-size: 48px;
-    //     font-weight: 300;
-    //     line-height: 72px;
-    //     letter-spacing: 0em;
-    //     text-align: left;
-    //     padding: 0px 5px;
-    //     justify-content: center;
-    //     align-items: center;
-    //     gap: 10px;
-    // }
-
-    // .newsTitle {
-    //     display: inline-flex;
-    //     padding: 0px 5px;
-    //     justify-content: center;
-    //     align-items: center;
-    //     gap: 10px;
-    // }
-
-    // .arrow {
-    //     border-radius: 25px;
-    //     background: #FFF;
-    //     box-shadow: 0px 0px 4px 0px rgba(0, 0, 0, 0.25);
-    //     width: 20px;
-    //     height: 20px;
-    // }
-
-    .containerNews> :nth-child(1) {
-        grid-area: 1 / 1 / 3 / 3;
-    }
-
-    .containerNews> :nth-child(2) {
-        grid-area: 1 / 3 / 2 / 4;
-    }
-
-    .containerNews> :nth-child(3) {
-        grid-area: 2 / 3 / 3 / 4;
-    }
-
+     .h1 {
+         font-size: 1.5rem;
+         text-decoration: none;
+         font-family: Urbanist;
+         font-size: 48px;
+         font-weight: 300;
+         line-height: 72px;
+         letter-spacing: 0em;
+         text-align: left;
+         padding: 0px 5px;
+         justify-content: center;
+         align-items: center;
+         gap: 10px;
+     }
+     .newsTitle {
+         display: inline-flex;
+         padding: 0px 5px;
+         justify-content: center;
+         align-items: center;
+         gap: 10px;
+     }
+     .arrow {
+         border-radius: 25px;
+         background: #FFF;
+         box-shadow: 0px 0px 4px 0px rgba(0, 0, 0, 0.25);
+         width: 20px;
+         height: 20px;
+     }
 </style>
 
 <div class="containerNews">
