1abc4ee83f2979175b2faa6c39d95d55316c8dcc
ReddiJe <nikitosciba@gmail.com>
2023-11-05 19:08:19 +0100

    wpCommit

--- themes/hello-elementor/news.php
+++ themes/hello-elementor/news.php
@@ -16,10 +16,36 @@ $postslist = get_posts([
 ]);
 ?>
 <style>
+
+    .h1 {
+        color: var(--Black, #2C2D2C);
+        text-align: right;
+        font-family: NewYork;
+        font-size: 48px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: normal;
+        text-transform: uppercase;
+    }
+
     .oneNews {
         padding: 100px;
         width: 100%;
         background-size: cover;
+        border-radius: 25px;
+        text-decoration: none;
+        padding: 20px;
+    }
+
+    .titleForImage {
+        position: relative;
+        display: inline-flex;
+        padding: 0px 5px;
+        justify-content: center;
+        align-items: center;
+        gap: 10px;
+        border-radius: 0px 0px 15px 15px;
+        background: #FFF;
     }
 
     .containerNews {
@@ -32,6 +58,17 @@ $postslist = get_posts([
         grid-template-columns: repeat(3, 1fr);
         grid-template-rows: repeat(2, 1fr);
     }
+
+    .arrowButton {
+    position: absolute; 
+    bottom: 0; 
+    right: 0; 
+    background-color: #fff; 
+    color: #000; 
+    border: black; 
+    border-radius: 25px;
+    cursor: pointer; 
+}
 
     .containerNews> :nth-child(1) {
         /* Styles for .div1 */
@@ -67,8 +104,13 @@ $postslist = get_posts([
     ?>
 
             <a href="<?php the_permalink(); ?>" class="oneNews" style="background-image: url(<?php echo get_the_post_thumbnail_url(); ?>);">
-                <h1><?php the_title(); ?></h1>
-                <div class="arrow"></div>
+                <h2 class="titleForImage"><?php the_title(); ?></h2>
+                <div class="arrow"><button class="arrowButton"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g id="iconoir:arrow-tr">
+<path id="Vector" d="M6 19L19 6M19 6V18.48M19 6H6.52" stroke="#021822" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+</g>
+</svg>
+</button></div>
             </a>
 
 
@@ -82,8 +124,7 @@ $postslist = get_posts([
 </div>
 
 <div class="containerButton">
-
-
+        <a href="#" class="view-all-button">More News</a>
 </div>
 
 <?php get_footer(); ?>
